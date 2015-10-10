d33dfb7d181d5de2328f5ea3e5d8b4869da4cc9c
powerj-sk <johny.valentik@gmail.com>
2015-10-10 12:08:57 +0200

    first commit

---
+++ src/main/java/org/pwj/HelloWorld.java
@@ -0,0 +1,10 @@
+package org.pwj;
+
+/**
+ * Created by Jan Valentik on 10/10/2015.
+ */
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+	}
+}
